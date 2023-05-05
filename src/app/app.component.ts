6bc62755608becfbe0df431b7535c76bc3455e44
rutuja <rutuja.chandgude@anka.co.in>
2023-05-05 11:02:02 +0530

    Added my project

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue;
+  //   }
+  // `]
+})
+export class AppComponent {
+  // name(name: any) {
+  //   throw new Error('Method not implemented.');
+  // }
+
+  // name(name: any) {
+  //   throw new Error('Method not implemented.');
+  // }
+  // username = '';
+
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+}
